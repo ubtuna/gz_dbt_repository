99a20000f6784c907e2e0ae16549940afe84b240
Uğur Batuhan Tuna <ubtuna@gmail.com>
2026-02-10 14:11:41 +0000

    1

--- models/staging/raw/stg_raw__sales.sql
+++ models/staging/raw/stg_raw__sales.sql
@@ -1,22 +1,15 @@
-with 
-
-source as (
-
-    select * from {{ source('raw', 'sales') }}
-
-),
-
-renamed as (
-
-    select
-        date_date,
-        orders_id,
-        products_id,
-        revenue,
-        quantity
-
-    from source
-
-)
-
+-- stg_raw__sales.sql
+with
+ source as (
+  select * from {{ source('raw', 'sales') }}
+ ),
+ renamed as (
+  select
+   date_date,
+   orders_id,
+   products_id,
+   revenue,
+   quantity
+  from source
+ )
 select * from renamed
