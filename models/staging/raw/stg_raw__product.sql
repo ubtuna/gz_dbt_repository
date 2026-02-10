4187e3c5574babd0e514a1b3a55da7398e5881bc
Uğur Batuhan Tuna <ubtuna@gmail.com>
2026-02-10 14:05:35 +0000

    1

--- models/staging/raw/stg_raw__product.sql
+++ models/staging/raw/stg_raw__product.sql
@@ -10,7 +10,7 @@ renamed as (
 
     select
         products_id,
-        CAST(purchse_price as FLOAT64) as purchase_price
+        CAST(purchase_price as FLOAT64) as purchase_price
 
     from source
 
